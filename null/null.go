2d4056d178255062e909b2a4298ea2b23261a926
Toby Burress <kurin@google.com>
2016-08-28 23:36:39 -0400

    null: add a null erasure code.

---
+++ null/null.go
@@ -0,0 +1,31 @@
+// Package null provides a null encoding for erasure.
+package null
+
+// Code satisfies erasure.Code, and splits the incoming stream into a number of
+// shards.  Code provides no redundancy.
+type Code int
+
+func (n Code) Encode(p []byte) [][]byte {
+	var bs [][]byte
+	size := (len(p) + int(n-1)) / int(n)
+	for i := 0; i < int(n); i++ {
+		if len(p) >= (i+1)*size {
+			bs = append(bs, p[i*size:(i+1)*size])
+		} else {
+			bs = append(bs, make([]byte, size))
+		}
+	}
+	return bs
+}
+
+func (n Code) Decode(ps [][]byte) ([]byte, error) {
+	var p []byte
+	for i := range ps {
+		p = append(p, ps[i]...)
+	}
+	return p, nil
+}
+
+func (n Code) Shards() int {
+	return int(n)
+}
